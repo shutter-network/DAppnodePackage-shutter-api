8b0f92b454fc5ab9ec4fecaa67c8c86c2ecf6a41
blockchainluffy <jainpunit1997@gmail.com>
2025-02-25 20:10:08 +0530

    use same ports for host and containers, update for chain's p2p port

--- shutter/go-shutter-settings/settings/chain.go
+++ shutter/go-shutter-settings/settings/chain.go
@@ -8,26 +8,27 @@ import (
 )
 
 type ChainConfig struct {
-	Moniker    string `env:"KEYPER_NAME"`
-    Genesis_file string `env:"ASSETS_GENESIS_FILE"`
-    P2P struct {
-        Seeds string `env:"_ASSETS_SHUTTERMINT_SEED_NODES"`
-        External_address string `env:"SHUTTER_EXTERNAL_ADDRESS"`
-        Addr_book_strict bool `env:"SHUTTER_ADDR_BOOK_STRICT"`
-        Pex bool `env:"SHUTTER_P2P_PEX"`
-    }
-    Instrumentation struct {
-        Prometheus bool `env:"SHUTTER_PUSH_METRICS_ENABLED"`
-        Prometheus_listen_addr string `env:"SHUTTER_PROMETHEUS_LISTEN_ADDR"`
-    }
+	Moniker      string `env:"KEYPER_NAME"`
+	Genesis_file string `env:"ASSETS_GENESIS_FILE"`
+	P2P          struct {
+		Seeds            string `env:"_ASSETS_SHUTTERMINT_SEED_NODES"`
+		External_address string `env:"SHUTTER_EXTERNAL_ADDRESS"`
+		Addr_book_strict bool   `env:"SHUTTER_ADDR_BOOK_STRICT"`
+		Pex              bool   `env:"SHUTTER_P2P_PEX"`
+		Laddr            string `env:"SHUTTER_P2P_LADDR"`
+	}
+	Instrumentation struct {
+		Prometheus             bool   `env:"SHUTTER_PUSH_METRICS_ENABLED"`
+		Prometheus_listen_addr string `env:"SHUTTER_PROMETHEUS_LISTEN_ADDR"`
+	}
 }
 
 func AddSettingsToChain(generatedFilePath, outputFilePath string) error {
-    var generatedConfig map[string]interface{}
+	var generatedConfig map[string]interface{}
 
-    fmt.Println("Adding user settings to chain...")
+	fmt.Println("Adding user settings to chain...")
 
-    if _, err := os.Stat(generatedFilePath); os.IsNotExist(err) {
+	if _, err := os.Stat(generatedFilePath); os.IsNotExist(err) {
 		return fmt.Errorf("generated file does not exist: %s", generatedFilePath)
 	}
 
@@ -36,17 +37,17 @@ func AddSettingsToChain(generatedFilePath, outputFilePath string) error {
 		return err
 	}
 
-    chainConfig := getChainConfigFromEnvs()
+	chainConfig := getChainConfigFromEnvs()
 
-    // ToLower is used because chain cofig file fields are lower case, but the struct
-    // fields are upper case to be exported
-    ApplyConfigToGenerated(reflect.ValueOf(chainConfig), &generatedConfig, strings.ToLower)
+	// ToLower is used because chain cofig file fields are lower case, but the struct
+	// fields are upper case to be exported
+	ApplyConfigToGenerated(reflect.ValueOf(chainConfig), &generatedConfig, strings.ToLower)
 
-    MarshalToFile(outputFilePath, generatedConfig)
+	MarshalToFile(outputFilePath, generatedConfig)
 
-    fmt.Println("Chain TOML file modified successfully and saved to", outputFilePath)
+	fmt.Println("Chain TOML file modified successfully and saved to", outputFilePath)
 
-    return nil
+	return nil
 }
 
 func getChainConfigFromEnvs() ChainConfig {

4c00dc12bbe0ff374bc8c6c74dd4486d0a042af9
Konrad Feldmeier <konrad@brainbot.com>
2025-04-08 15:26:47 +0200

    Fix script path

--- shutter/scripts/run_configure.sh
+++ shutter/scripts/run_configure.sh
@@ -3,7 +3,7 @@
 
 if [[ SHUTTER_PUSH_LOGS_ENABLED=true ]];
 then
-    ./configure.sh | rotatelogs -n 1 -e -c /tmp/configure.log 5M
+    configure.sh | rotatelogs -n 1 -e -c /tmp/configure.log 5M
 else
-    ./configure.sh
+    configure.sh
 fi

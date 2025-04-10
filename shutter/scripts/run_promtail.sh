0da6c1855ccb3973a9f90abea6521e1fb4e71ec2
Konrad Feldmeier <konrad@brainbot.com>
2025-04-10 10:03:22 +0000

    Expand env in promtail config

--- shutter/scripts/run_promtail.sh
+++ shutter/scripts/run_promtail.sh
@@ -3,7 +3,7 @@
 run_promtail() {
     if [[ $SHUTTER_PUSH_LOGS_ENABLED=true ]];
     then
-        promtail -log-config-reverse-order -print-config-stderr -config.file /etc/promtail_config.yaml
+        promtail -config.expand-env=true -log-config-reverse-order -print-config-stderr -config.file /etc/promtail_config.yaml
     else
         tail -f /dev/null
     fi

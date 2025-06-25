8105b6232b66dbdff044bdd0a4ddf654a2fa88c8
blockchainluffy <jainpunit1997@gmail.com>
2025-06-25 18:06:00 +0530

    fix: add staker-scripts@v0.1.0 with exlicit 8545 port check

--- shutter/scripts/configure_keyper.sh
+++ shutter/scripts/configure_keyper.sh
@@ -16,7 +16,7 @@ PATH=$NODE_HOME/bin:$PATH
 function test_ethereum_url() {
     # FIXME: This is a workaround for the issue with the staker-scripts@v0.1.1 not setting get_execution_ws_url_from_global_env correctly in the environment variables.
     # Git Issue: https://github.com/dappnode/staker-package-scripts/issues/11
-    export SHUTTER_NETWORK_NODE_ETHEREUMURL=${ETHEREUM_WS:-ws://execution.${NETWORK}.dncore.dappnode:8546}
+    export SHUTTER_NETWORK_NODE_ETHEREUMURL=${ETHEREUM_WS:-$(get_execution_ws_url_from_global_env ${NETWORK} ${SUPPORTED_NETWORKS})}
     RESULT=$(wscat -c "$SHUTTER_NETWORK_NODE_ETHEREUMURL" -x '{"jsonrpc": "2.0", "method": "eth_syncing", "params": [], "id": 1}')
     if [[ $RESULT =~ '"id":1' ]]; then return 0; else
         export SHUTTER_NETWORK_NODE_ETHEREUMURL=ws://execution.${NETWORK}.dncore.dappnode:8545
